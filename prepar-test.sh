e195b1c5fdac8bfd0e24c67a6434a82ec1542323
duerpei <duep.fnst@fujitsu.com>
2022-12-16 15:32:41 +0800

    Add ls debug command
    Signed-off-by: duerpei <duep.fnst@fujitsu.com>

--- prepar-test.sh
+++ prepar-test.sh
@@ -22,6 +22,8 @@ rpm -ivh --force agl-test-framework-1-r0.aarch64.rpm
 
 mkdir -p /var/run/agl-test/logs/log-to-report/
 
-tree /usr/AGL/agl-test/
+ls /usr/AGL/agl-test/
+ls /usr/AGL/agl-test/tests/
+ls /usr/AGL/agl-test/plugins/
 
 ptest-runner -l
